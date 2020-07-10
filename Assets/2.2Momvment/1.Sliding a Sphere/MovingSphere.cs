83922f7ec9dc74f6f9c6af85ad6eff5e8abd7c37
circXsora <circXsora@outlook.com>
2020-07-10 23:58:35 +0800

    Desired Velocity with limited acceleration

--- Assets/2.2Momvment/1.Sliding a Sphere/MovingSphere.cs
+++ Assets/2.2Momvment/1.Sliding a Sphere/MovingSphere.cs
@@ -2,6 +2,30 @@
 
 public class MovingSphere : MonoBehaviour
 {
+	[SerializeField, Range(0f, 100f)]
+	float maxSpeed = 10f;
 
+	Vector3 velocity;
 
+	[SerializeField, Range(0f, 100f)]
+	float maxAcceleration = 10f;
+
+	void Update()
+	{
+		Vector2 playerInput;
+		playerInput.x = Input.GetAxis("Horizontal");
+		playerInput.y = Input.GetAxis("Vertical");
+		playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+		Vector3 desiredVelocity =
+			new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+		float maxSpeedChange = maxAcceleration * Time.deltaTime;
+		velocity.x =
+			Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+		velocity.z =
+			Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
+		Vector3 displacement = velocity * Time.deltaTime;
+		transform.localPosition += displacement;
+	}
 }
