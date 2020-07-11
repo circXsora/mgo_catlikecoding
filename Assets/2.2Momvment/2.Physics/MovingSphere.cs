18b98fd816dfd0d2fd9ad5caf2b099c629955707
circXsora <circXsora@outlook.com>
2020-07-11 21:33:05 +0800

    finally

--- Assets/2.2Momvment/2.Physics/MovingSphere.cs
+++ Assets/2.2Momvment/2.Physics/MovingSphere.cs
@@ -1,129 +1,160 @@
 ﻿using UnityEngine;
 
-namespace _2_2
+public class MovingSphere : MonoBehaviour
 {
 
-    public class MovingSphere : MonoBehaviour
-    {
+	[SerializeField, Range(0f, 100f)]
+	float maxSpeed = 10f;
 
-        [SerializeField, Range(0f, 100f)]
-        float maxSpeed = 10f;
+	[SerializeField, Range(0f, 100f)]
+	float maxAcceleration = 10f, maxAirAcceleration = 1f;
 
-        Vector3 velocity, desiredVelocity;
-		bool desiredJump;
+	[SerializeField, Range(0f, 10f)]
+	float jumpHeight = 2f;
 
-        [SerializeField, Range(0f, 100f)]
-        float maxAcceleration = 10f, maxAirAcceleration = 1f;
+	[SerializeField, Range(0, 5)]
+	int maxAirJumps = 0;
 
-		[SerializeField, Range(0f, 10f)]
-		float jumpHeight = 2f;
+	[SerializeField, Range(0, 90)]
+	float maxGroundAngle = 25f;
 
-		Rigidbody body;
+	Rigidbody body;
 
-		[SerializeField, Range(0, 5)]
-		int maxAirJumps = 0;
+	Vector3 velocity, desiredVelocity;
 
-		int jumpPhase;
+	Vector3 contactNormal;
 
-		[SerializeField, Range(0f, 90f)]
-		float maxGroundAngle = 25f;
+	bool desiredJump;
 
-		float minGroundDotProduct;
+	int groundContactCount;
 
-		void OnValidate()
+	bool OnGround => groundContactCount > 0;
+
+	int jumpPhase;
+
+	float minGroundDotProduct;
+
+	void OnValidate()
+	{
+		minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+	}
+
+	void Awake()
+	{
+		body = GetComponent<Rigidbody>();
+		OnValidate();
+	}
+
+	void Update()
+	{
+		Vector2 playerInput;
+		playerInput.x = Input.GetAxis("Horizontal");
+		playerInput.y = Input.GetAxis("Vertical");
+		playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+		desiredVelocity =
+			new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+
+		desiredJump |= Input.GetButtonDown("Jump");
+	}
+
+	void FixedUpdate()
+	{
+		UpdateState();
+		AdjustVelocity();
+
+		if (desiredJump)
 		{
-			minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+			desiredJump = false;
+			Jump();
 		}
 
-		void Awake()
-        {
-            body = GetComponent<Rigidbody>();
-			OnValidate();
-		}
-		void Update()
+		body.velocity = velocity;
+		ClearState();
+	}
+
+	void ClearState()
+	{
+		groundContactCount = 0;
+		contactNormal = Vector3.zero;
+	}
+
+	void UpdateState()
+	{
+		velocity = body.velocity;
+		if (OnGround)
 		{
-			Vector2 playerInput;
-			playerInput.x = Input.GetAxis("Horizontal");
-			playerInput.y = Input.GetAxis("Vertical");
-			playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-
-			desiredJump |= Input.GetButtonDown("Jump");
-
-			desiredVelocity =
-				new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
-		}
-
-		void UpdateState()
-		{
-			velocity = body.velocity;
-			if (onGround)
+			jumpPhase = 0;
+			if (groundContactCount > 1)
 			{
-				jumpPhase = 0;
-			}
-            else
-            {
-				contactNormal = Vector3.up;
-            }
-		}
-
-		void FixedUpdate()
-		{
-			UpdateState();
-
-			if (desiredJump)
-            {
-				desiredJump = false;
-				Jump();
-            }
-			float acceleration = onGround ? maxAcceleration : maxAirAcceleration;
-			float maxSpeedChange = acceleration * Time.deltaTime;
-			velocity.x =
-				Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-			velocity.z =
-				Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
-			body.velocity = velocity;
-
-			onGround = false;
-		}
-
-		void Jump()
-        {
-            if (onGround || jumpPhase < maxAirJumps)
-			{
-				jumpPhase += 1;
-				float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
-				float alignedSpeed = Vector3.Dot(velocity, contactNormal);
-				if (alignedSpeed > 0f)
-				{
-					jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
-				}
-				velocity += contactNormal * jumpSpeed;
+				contactNormal.Normalize();
 			}
 		}
-
-		bool onGround;
-
-		void OnCollisionEnter(Collision collision)
+		else
 		{
-			EvaluateCollision(collision);
+			contactNormal = Vector3.up;
 		}
+	}
 
-		void OnCollisionStay(Collision collision)
+	void AdjustVelocity()
+	{
+		Vector3 xAxis = ProjectOnContactPlane(Vector3.right).normalized;
+		Vector3 zAxis = ProjectOnContactPlane(Vector3.forward).normalized;
+
+		float currentX = Vector3.Dot(velocity, xAxis);
+		float currentZ = Vector3.Dot(velocity, zAxis);
+
+		float acceleration = OnGround ? maxAcceleration : maxAirAcceleration;
+		float maxSpeedChange = acceleration * Time.deltaTime;
+
+		float newX =
+			Mathf.MoveTowards(currentX, desiredVelocity.x, maxSpeedChange);
+		float newZ =
+			Mathf.MoveTowards(currentZ, desiredVelocity.z, maxSpeedChange);
+
+		velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
+	}
+
+	void Jump()
+	{
+		if (OnGround || jumpPhase < maxAirJumps)
 		{
-			EvaluateCollision(collision);
-		}
-		Vector3 contactNormal;
-		void EvaluateCollision(Collision collision) {
-			for (int i = 0; i < collision.contactCount; i++)
+			jumpPhase += 1;
+			float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+			float alignedSpeed = Vector3.Dot(velocity, contactNormal);
+			if (alignedSpeed > 0f)
 			{
-				Vector3 normal = collision.GetContact(i).normal;
+				jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
+			}
+			velocity += contactNormal * jumpSpeed;
+		}
+	}
 
-				if (normal.y >= minGroundDotProduct)
-				{
-					onGround = true;
-					contactNormal = normal;
-				}
+	void OnCollisionEnter(Collision collision)
+	{
+		EvaluateCollision(collision);
+	}
+
+	void OnCollisionStay(Collision collision)
+	{
+		EvaluateCollision(collision);
+	}
+
+	void EvaluateCollision(Collision collision)
+	{
+		for (int i = 0; i < collision.contactCount; i++)
+		{
+			Vector3 normal = collision.GetContact(i).normal;
+			if (normal.y >= minGroundDotProduct)
+			{
+				groundContactCount += 1;
+				contactNormal += normal;
 			}
 		}
+	}
+
+	Vector3 ProjectOnContactPlane(Vector3 vector)
+	{
+		return vector - contactNormal * Vector3.Dot(vector, contactNormal);
 	}
 }
