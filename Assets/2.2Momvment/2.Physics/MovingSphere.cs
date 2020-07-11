39b143936c900cc6913c35caabafdb8c4e8b7c49
circXsora <circXsora@outlook.com>
2020-07-11 17:26:06 +0800

    Jump When Plane

--- Assets/2.2Momvment/2.Physics/MovingSphere.cs
+++ Assets/2.2Momvment/2.Physics/MovingSphere.cs
@@ -9,37 +9,78 @@ namespace _2_2
         [SerializeField, Range(0f, 100f)]
         float maxSpeed = 10f;
 
-        Vector3 velocity;
+        Vector3 velocity, desiredVelocity;
+		bool desiredJump;
 
         [SerializeField, Range(0f, 100f)]
         float maxAcceleration = 10f;
 
-        Rigidbody body;
+		[SerializeField, Range(0f, 10f)]
+		float jumpHeight = 2f;
+
+		Rigidbody body;
 
         void Awake()
         {
             body = GetComponent<Rigidbody>();
         }
+		void Update()
+		{
+			Vector2 playerInput;
+			playerInput.x = Input.GetAxis("Horizontal");
+			playerInput.y = Input.GetAxis("Vertical");
+			playerInput = Vector2.ClampMagnitude(playerInput, 1f);
 
-        void Update()
+			desiredJump |= Input.GetButtonDown("Jump");
+
+			desiredVelocity =
+				new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+		}
+
+		void FixedUpdate()
+		{
+			velocity = body.velocity;
+            if (desiredJump)
+            {
+				desiredJump = false;
+				Jump();
+            }
+			float maxSpeedChange = maxAcceleration * Time.deltaTime;
+			velocity.x =
+				Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+			velocity.z =
+				Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+			body.velocity = velocity;
+
+			onGround = false;
+		}
+
+		void Jump()
         {
-            Vector2 playerInput;
-            playerInput.x = Input.GetAxis("Horizontal");
-            playerInput.y = Input.GetAxis("Vertical");
-            playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+            if (onGround)
+            {
+				velocity.y += Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+			}
+		}
 
-            Vector3 desiredVelocity =
-                new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+		bool onGround;
 
-            velocity = body.velocity;
+		void OnCollisionEnter(Collision collision)
+		{
+			EvaluateCollision(collision);
+		}
 
-            float maxSpeedChange = maxAcceleration * Time.deltaTime;
-            velocity.x =
-                Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-            velocity.z =
-                Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+		void OnCollisionStay(Collision collision)
+		{
+			EvaluateCollision(collision);
+		}
 
-            body.velocity = velocity;
-        }
-    }
+		void EvaluateCollision(Collision collision) {
+			for (int i = 0; i < collision.contactCount; i++)
+			{
+				Vector3 normal = collision.GetContact(i).normal;
+				onGround |= normal.y >= 0.9f;
+			}
+		}
+	}
 }
