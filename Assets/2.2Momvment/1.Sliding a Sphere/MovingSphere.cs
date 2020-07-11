5b9d71e2b95c7289bc8d9e805ccbf9522e46c41f
circXsora <circXsora@outlook.com>
2020-07-11 16:44:34 +0800

    physical velocity

--- Assets/2.2Momvment/1.Sliding a Sphere/MovingSphere.cs
+++ Assets/2.2Momvment/1.Sliding a Sphere/MovingSphere.cs
@@ -1,67 +1,71 @@
 ﻿using UnityEngine;
 
-public class MovingSphere : MonoBehaviour
+namespace _2_1
 {
-    [SerializeField]
-    Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
 
-    [SerializeField, Range(0f, 100f)]
-    float maxSpeed = 10f;
-
-    Vector3 velocity;
-
-    [SerializeField, Range(0f, 100f)]
-    float maxAcceleration = 10f;
-
-    [SerializeField, Range(0f, 1f)]
-    float bounciness = 0.5f;
-
-    void Update()
+    public class MovingSphere : MonoBehaviour
     {
-        Vector2 playerInput;
-        playerInput.x = Input.GetAxis("Horizontal");
-        playerInput.y = Input.GetAxis("Vertical");
-        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        [SerializeField]
+        Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
 
-        Vector3 desiredVelocity =
-            new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
-        float maxSpeedChange = maxAcceleration * Time.deltaTime;
-        velocity.x =
-            Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-        velocity.z =
-            Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+        [SerializeField, Range(0f, 100f)]
+        float maxSpeed = 10f;
 
-        Vector3 displacement = velocity * Time.deltaTime;
-        Vector3 newPosition = transform.localPosition + displacement;
-        /*
-        if (!allowedArea.Contains(new Vector2(newPosition.x, newPosition.z)))
+        Vector3 velocity;
+
+        [SerializeField, Range(0f, 100f)]
+        float maxAcceleration = 10f;
+
+        [SerializeField, Range(0f, 1f)]
+        float bounciness = 0.5f;
+
+        void Update()
         {
-            newPosition.x =
-                Mathf.Clamp(newPosition.x, allowedArea.xMin, allowedArea.xMax);
-            newPosition.z =
-                Mathf.Clamp(newPosition.z, allowedArea.yMin, allowedArea.yMax);
+            Vector2 playerInput;
+            playerInput.x = Input.GetAxis("Horizontal");
+            playerInput.y = Input.GetAxis("Vertical");
+            playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+            Vector3 desiredVelocity =
+                new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+            float maxSpeedChange = maxAcceleration * Time.deltaTime;
+            velocity.x =
+                Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+            velocity.z =
+                Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
+            Vector3 displacement = velocity * Time.deltaTime;
+            Vector3 newPosition = transform.localPosition + displacement;
+            /*
+            if (!allowedArea.Contains(new Vector2(newPosition.x, newPosition.z)))
+            {
+                newPosition.x =
+                    Mathf.Clamp(newPosition.x, allowedArea.xMin, allowedArea.xMax);
+                newPosition.z =
+                    Mathf.Clamp(newPosition.z, allowedArea.yMin, allowedArea.yMax);
+            }
+            */
+            if (newPosition.x < allowedArea.xMin)
+            {
+                newPosition.x = allowedArea.xMin;
+                velocity.x = -velocity.x * bounciness;
+            }
+            else if (newPosition.x > allowedArea.xMax)
+            {
+                newPosition.x = allowedArea.xMax;
+                velocity.x = -velocity.x * bounciness;
+            }
+            if (newPosition.z < allowedArea.yMin)
+            {
+                newPosition.z = allowedArea.yMin;
+                velocity.z = -velocity.z * bounciness;
+            }
+            else if (newPosition.z > allowedArea.yMax)
+            {
+                newPosition.z = allowedArea.yMax;
+                velocity.z = -velocity.z * bounciness;
+            }
+            transform.localPosition = newPosition;
         }
-        */
-        if (newPosition.x < allowedArea.xMin)
-        {
-            newPosition.x = allowedArea.xMin;
-            velocity.x = -velocity.x * bounciness;
-        }
-        else if (newPosition.x > allowedArea.xMax)
-        {
-            newPosition.x = allowedArea.xMax;
-            velocity.x = -velocity.x * bounciness;
-        }
-        if (newPosition.z < allowedArea.yMin)
-        {
-            newPosition.z = allowedArea.yMin;
-            velocity.z = -velocity.z * bounciness;
-        }
-        else if (newPosition.z > allowedArea.yMax)
-        {
-            newPosition.z = allowedArea.yMax;
-            velocity.z = -velocity.z * bounciness;
-        }
-        transform.localPosition = newPosition;
     }
 }
---
+++ Assets/2.2Momvment/2.Physics/MovingSphere.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace _2_2
+{
+
+    public class MovingSphere : MonoBehaviour
+    {
+
+        [SerializeField, Range(0f, 100f)]
+        float maxSpeed = 10f;
+
+        Vector3 velocity;
+
+        [SerializeField, Range(0f, 100f)]
+        float maxAcceleration = 10f;
+
+        Rigidbody body;
+
+        void Awake()
+        {
+            body = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            Vector2 playerInput;
+            playerInput.x = Input.GetAxis("Horizontal");
+            playerInput.y = Input.GetAxis("Vertical");
+            playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+            Vector3 desiredVelocity =
+                new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+
+            velocity = body.velocity;
+
+            float maxSpeedChange = maxAcceleration * Time.deltaTime;
+            velocity.x =
+                Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+            velocity.z =
+                Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
+            body.velocity = velocity;
+        }
+    }
+}
